1338e2bb4b7b67052645db0f9bac6d1eed15175e
milenasandra <zsandra745@gmail.com>
2022-10-11 21:16:12 -0500

    Proyecto finalizado

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require('express')
-
+const hbs = require('hbs')
+require('dotenv').config()
 const app = express()
-
-const port = 8013
+const port = process.env.PORT
 
 app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', {
+    nombre: 'Sandra Zúñiga',
+    titulo: 'Módulo Node JS',
+  })
 })
 
 app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html')
+  res.render('generic')
 })
 
 app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html')
+  res.render('elements')
 })
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/404.html')
+  res.render('404.hbs')
 })
 
 app.listen(port, () => {
